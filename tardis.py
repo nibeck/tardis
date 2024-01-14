ef4f2cbce64ae5d0ac05707e8a9a4039000790e1
nibeck <nibeck@mac.com>
2024-01-14 12:30:26 -0500

    Another publish

--- tardis.py
+++ tardis.py
@@ -3,16 +3,55 @@ import math
 from rpi_ws281x import *
 import colorsys
 
+# TODO: Expose as restful services
+# TODO: redo get() set() properly for tardis oject
+
+#setup some comnstants
+LED_COUNT      = 40         # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10     # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
+LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+BLUE    = Color(0,0,255)
+BLACK   = Color(0,0,0)
+WHITE   = Color(255,255,255)
+RED     = Color(255,0,0)
+PURPLE  = Color(127,0,255)
+YELLOW  = Color(255,255,0)
+GREEN   = Color(50,205,50)
+TEAL    = Color(100,128,128)
+NAVY    = Color(0,0,128)
+
 class TARDIS:
     def __init__(self, doctor, color):
         self.doctor = doctor if doctor is not None else ""
         self.color = color if color is not None else Color(0,0,0) 
-        # Windows
-        self.frontWindow = Window("front", Color(255,255,255), 50)
-        self.backWindow = Window("back", Color(255,255,255), 50)
-        self.rtWindow = Window("right", Color(255,255,255), 50)
-        self.leftWindow = Window("left", Color(255,255,255), 50)
-        # Signs
+        # Create the 4 Windows
+        self.frontWindow = Window()
+        self.frontWindow.location = "front"
+        self.frontWindow.color = Color(0,0,0)
+        # self.frontWindow.brightness = 50
+
+        self.backWindow = Window()
+        self.backWindow.location = "back"
+        self.backWindow.color = Color(0,0,0)
+        # self.backWindow.brightness = 50
+
+        self.rtWindow = Window()
+        self.rtWindow.location = "right"
+        self.rtWindow.color = Color(0,0,0)
+        # self.rtWindow.brightness = 50
+
+        self.leftWindow = Window()
+        self.leftWindow.location = "left"
+        self.leftWindow.color = Color(0,0,0)
+        # self.leftWindow.brightness = 50
+
+        # Create the 4 Signs
+        # TODO: Replace getters/setters with property
         self.frontSign = Sign(Color(255,255,255), 0)
         self.backSign = Sign(Color(255,255,255), 0)
         self.rtSign = Sign(Color(255,255,255), 0)
@@ -24,25 +63,84 @@ class TARDIS:
         # initialize light strip
         self.lightStrip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         self.lightStrip.begin()
-
+        
     def turnOff(self):
         self.frontWindow.turnOff()
         self.backWindow.turnOff()
         self.rtWindow.turnOff()
         self.leftWindow.turnOff()
+    
     def turnOn(self):
-        self.frontWindow.turnOn()
-        self.backWindow.turnOn()
-        self.rtWindow.turnOn()
-        self.leftWindow.turnOn()
+        self.frontWindow.setColor(WHITE)
+        self.backWindow.setColor(WHITE)
+        self.rtWindow.setColor(WHITE)
+        self.leftWindow.setColor(WHITE)
+    
+    def rainbow(self):
+        self.frontWindow.setColor(RED)
+        self.backWindow.setColor(PURPLE)
+        self.rtWindow.setColor(TEAL)
+        self.leftWindow.setColor(NAVY)
+    
+    def flash(self):
+            self.backSign
 
 class Window:
-    def __init__(self, location, color, brightness):
-        self.color = color if color is not None else Color(255,255,255)
-        self.location = location if location is not None else "front"
-        self.brightness = brightness if brightness is not None else 50
-        # add logic to look at location and properly set a new attribute called LEDindex to the approate indices.
-        match self.location:
+    def __init__(self):
+        self._color = Color(0,0,0)
+        self._brightness = 50
+
+    @property
+    def color(self):
+        return self._color
+    @color.setter    
+    def color(self, new_color):
+        self._color = new_color
+        self.updateLED_color()
+
+    @property
+    def brightness(self):
+        return self._brightness    
+    @brightness.setter
+    def brightness(self, new_brightness):
+        # ensure brightness apssed in is betwen 0 and 100
+        new_brightness = max(min(new_brightness, 100), 0)
+    
+        # Perform linear mapping - map to 0-50 (50=maximum brightness)
+        # allow user to pass in 100 as max as that feels more normal. 
+        # do this mapping interannly to increase ease of use
+        #       (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min        
+        mapped_value = (new_brightness - 0) * (50 - 1) / (100 - 0) + 1
+        # Round the result to an integer (optional, depending on your needs)
+        mapped_value = int(round(mapped_value))
+
+        # nromalize RGB valies to prepare for the hls conversion
+        normalizedRed = self.color.r / 255
+        normalizedGreen = self.color.g / 255
+        normalizedBlue = self.color.b / 255
+
+        # convert RGB valiues into Hls values
+        hue, origLightness, saturation = colorsys.rgb_to_hls(normalizedRed, normalizedGreen, normalizedBlue)
+        # convert back to rgb usinf hls with new brightness values passed in (divide by 100 to put it inhls range (.1-1.0)
+        newR, newG, newB = colorsys.hls_to_rgb(hue, mapped_value/100, saturation)
+        # normalize new RGB valies from the HLS conversion 
+        newR = newR * 255
+        newG = newG * 255
+        newB = newB * 255
+
+        # set the new color of the window
+        adjustedColor = Color(math.trunc(newR), math.trunc(newG), math.trunc(newB))
+        self.color = adjustedColor
+        lightStrip.show()
+        self._brightness = new_brightness
+
+    @property
+    def location(self):
+        return self._location
+    @location.setter
+    def location(self, new_location):
+        self._location = new_location
+        match self._location:
             case "front":
                 self.ledIndices = range(10)        #0-9
             case "right":
@@ -51,106 +149,59 @@ class Window:
                 self.ledIndices = range(20, 29, 1) #20-29
             case "back":
                 self.ledIndices = range(30, 39, 1) #30-39
+            case _:
+                raise ValueError("Location must be front, right, back, left")
 
     def __str__(self):
         return "Color: {} Brightness: {} Location: {}".format(self.color, self.brightness, self.location)
 
     def turnOn(self):
-        self.updateLEDs()
+        lightStrip.show()
     
     def turnOff(self):   
-        self.setBrightness(0)
-        self.updateLEDs()
+        # Todo: Change to set to 0% brightness onmce brightness is working
+        self.color = Color(0,0,0)
+        self.updateLED_color()
+        lightStrip.show()
     
-    def updateLEDs(self):
+    def updateLED_color(self):
         for ix in self.ledIndices:
             lightStrip.setPixelColor(ix, self.color)    
-        lightStrip.show()
+        # lightStrip.show()
 
-    def setBrightness(self, percentage):
-        # clamp brightness percentage into 0-100 range
-        percentage = min(100, max(0, percentage))
-
-        # HSV: hue, saturation, value (0-255)
-        # normalize RGB valeue top work with HSV by dividing by 255
-        hue, saturation, colorValue = colorsys.rgb_to_hsv((self.color.r / 255), (self.color.g / 255), (self.color.b / 255) )
-        colorValue = colorValue * (percentage / 100)
-
-        # get new RGB valiues
-        newR, newG, newB = colorsys.hsv_to_rgb(hue, saturation, colorValue)
-
-        # set the new color of the window
-        # normalize the returned RGB value by multieplying by 255
-        adjustedColor = Color(math.trunc(newR*255), math.trunc(newG*255), math.trunc(newB*255))
-        self.setColor(adjustedColor)
-        self.brightness = percentage
-        self.updateLEDs()
-
-    def setColor(self, color):
-        self.color = color
-        self.updateLEDs()
-
-    def flash(self, cycles, speed):
+    def flash(self, cycles, delay):
         origColor = self.color
         for n in range(cycles):
-            time.sleep(speed)
-            self.setColor(origColor)
-            time.sleep(speed)
-            self.setBrightness(0)
-        self.setColor(origColor)
+            time.sleep(delay)
+            self.color = origColor
+            lightStrip.show()
+            time.sleep(delay)
+            self.brightness = 0
+            lightStrip.show()
+        self.color = origColor
 
-    def pulseWindow(self, delay, cycles):
-
-        cycles = min(100, max(1, cycles))
-
-        # HSV: hue, saturation, value (0-255)
-        # normalize RGB values for hsv by dividing by 255
-        hue, saturation, colorValue = colorsys.rgb_to_hsv(self.color.r / 255, self.color.g / 255, self.color.b / 255)
-
-        for cycle in range(cycles):
-            for colorValue in range(1, 256, 1):
-                # print("Going up: ", colorValue)
-                newR, newG, newB = colorsys.hsv_to_rgb(hue, saturation, colorValue) 
-                # normalize new RGB valies from the HLS conversion and set new color with increased limunance
-                newR = newR * 255
-                newG = newG * 255
-                newB = newB * 255
-                
-                # set the new color of the window
-                adjustedColor = Color(math.trunc(newR), math.trunc(newG), math.trunc(newB))
-                self.setColor(adjustedColor)
-                time.sleep(delay)
-            for colorValue in range(254, 1, -1):
-                # print("ColorValue going down: ", colorValue)    
-                newR, newG, newB = colorsys.hsv_to_rgb(hue, saturation, colorValue) 
-                # normalize new RGB valies from the HLS conversion and set new color with increased limunance
-                newR = newR * 255
-                newG = newG * 255
-                newB = newB * 255
-                
-                # set the new color of the window
-                adjustedColor = Color(math.trunc(newR), math.trunc(newG), math.trunc(newB))
-                self.setColor(adjustedColor)
-                time.sleep(delay)
+    def pulseWindow(self, cycles, delay):
+            # save starting point so we can return to it
+            starting_brightness = self.brightness
+            # iterate through changing brightness as we go
+            for cycles in range(cycles):
+                # start at current brightness and go down to 0
+                for newbrightness in range(starting_brightness, 1, -1):
+                    self.brightness = newbrightness
+                    self.turnOn()
+                    time.sleep(delay)
+                for newbrightness in range(1, starting_brightness, 1):
+                    self.brightness = newbrightness
+                    self.turnOn()
+                    time.sleep(delay)
 
 class Sign:
     def __init__(self, color, brightness):
         self.color = color if color is not None else Color(0.0,0)
         self.brightness = brightness if brightness is not None else 0
 
-#setup some comnstants
-LED_COUNT      = 40     # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
-LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
-
 # Create NeoPixel object with appropriate configuration.
 lightStrip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
-# # Intialize the library (must be called once before other functions).
+# Intialize the library (must be called once before other functions).
 lightStrip.begin()
--- tardisController.py
+++ tardisController.py
@@ -1,10 +1,14 @@
 #include all necessary packages to get LEDs to work with Raspberry Pi
 import time
 import math
+import signal
+import sys
 from rpi_ws281x import *
 import argparse
 import colorsys
 from tardis import TARDIS
+
+from flask import Flask
 
 BLUE    = Color(0,0,255)
 BLACK   = Color(0,0,0)
@@ -16,141 +20,41 @@ GREEN   = Color(50,205,50)
 TEAL    = Color(100,128,128)
 NAVY    = Color(0,0,128)
 
-def fadeWindow(window, delay, cycles):
-    brightnessPercentage = 100
-    startColor = window.color
-    startR = startColor.r 
-    startG = startColor.g 
-    startB = startColor.b 
+# TODO: Invetigate ruuning asyn or in parallel 
+# TODO: Wrap in a simple Web UI
 
-    while window.color != BLACK:
-        window.setBrightness(brightnessPercentage)
-        brightnessPercentage = brightnessPercentage - 1
-        myTARDIS.backWindow.updateLEDs()
-        time.sleep(delay)
-    brightnessPercentage = 100    
-    while window.color != startColor:
-        window.setBrightness(brightnessPercentage)
-        brightnessPercentage = brightnessPercentage + 1
-        
-        myTARDIS.backWindow.updateLEDs()
-        time.sleep(delay)
+# app = Flask(__name__)
 
-def rgb_to_hls1(rgb_color):
-    # Ensure that the RGB values are in the range [0, 255]
-    r, g, b = [min(255, max(0, int(x))) for x in rgb_color]
+# @app.route('/')
+# def hello_world():
+#     return 'Hello TARDIS !!!!'
 
-    # Normalize RGB values to the range [0, 1]
-    r_normalized = r / 255.0
-    g_normalized = g / 255.0
-    b_normalized = b / 255.0
+# main driver function
+# if __name__ == '__main__':
+ 
+#     run() method of Flask class runs the application 
+#     on the local development server.
+#     app.run()
 
-    # Convert RGB to HLS
-    h, l, s = colorsys.rgb_to_hls(r_normalized, g_normalized, b_normalized)
+# on ctrl-c, turn LEDs off
+def signal_handler(signal, frame):
+    myTARDIS.turnOff()  
+    sys.exit(0)
 
-    # Convert hue to degrees (0-360)
-    h_degrees = int(h * 360)
-
-    return {
-        'rgb': (r, g, b),
-        'hls': (h_degrees, l, s),
-        'hue': h_degrees,
-        'saturation': s,
-        'luminance': l
-    }
-
-def rgb_to_hls(r, g, b):
-    # Ensure that the RGB values are in the range [0, 255]
-    r = min(255, max(0, int(r)))
-    g = min(255, max(0, int(g)))
-    b = min(255, max(0, int(b)))
-
-    # Normalize RGB values to the range [0, 1]
-    r_normalized = r / 255.0
-    g_normalized = g / 255.0
-    b_normalized = b / 255.0
-
-    # Convert RGB to HLS
-    h, l, s = colorsys.rgb_to_hls(r_normalized, g_normalized, b_normalized)
-
-    # Convert hue to degrees (0-360)
-    h_degrees = int(h * 360)
-
-    return h_degrees, s, l
-
-def colorConversionTest():
-
-    r = 0
-    g = 0
-    b = 128
-    print('RGB:-')
-    print('r: ' + str(r) + '\ng: ' + str(g) + '\nb: ' + str(b))
-    ## normalizing values to get value between 0 and 1
-    r = r/255
-    g = g/255
-    b = b/255
-    h, l, s = colorsys.rgb_to_hls(r, g, b)
-    print('Normalized RGB:-')
-    print('r: ' + str(r) + '\ng: ' + str(g) + '\nb: ' + str(b))
-    print('\nHLS:-')
-    print('h: ' + str(h) + '\nl: ' + str(l) + '\ns: ' + str(s))
-    r, g, b = colorsys.hls_to_rgb(h, l, s)
-    print('\nRGB:-')
-    print('r: ' + str(r) + '\ng: ' + str(g) + '\nb: ' + str(b))
-    r = r * 255
-    g = g * 255
-    b = b * 255
-    print('\nNormalized RGB:-')
-    print('r: ' + str(r) + '\ng: ' + str(g) + '\nb: ' + str(b))
-
-def pulseLightness(delay):
-    # HLS: Hue, lightnmess, saturation
-    normalizedRed = myTARDIS.backWindow.color.r / 255
-    normalizedGreen = myTARDIS.backWindow.color.g / 255
-    normalizedBlue = myTARDIS.backWindow.color.b / 255
-    hue, lightness, saturation = colorsys.rgb_to_hls(normalizedRed, normalizedGreen, normalizedBlue)
-
-    while True:
-        for lightness in range(1, 101):
-            # convert back to rgb usinf hls with inctreased lightness
-            newR, newG, newB = colorsys.hls_to_rgb(hue, lightness/100, saturation) 
-            # normalize new RGB valies from the HLS conversion and set new color with increased limunance
-            newR = newR * 255
-            newG = newG * 255
-            newB = newB * 255
-            
-            # set the new color of the window
-            adjustedColor = Color(math.trunc(newR), math.trunc(newG), math.trunc(newB))
-            myTARDIS.backWindow.setColor(adjustedColor)
-            time.sleep(delay)
-        for lightness in range(100, 1, -1):
-            # convert back to rgb using hls with inctreased lightness
-            newR, newG, newB = colorsys.hls_to_rgb(hue, lightness/100, saturation) 
-            # normalize new RGB valies from the HLS conversion and set new color with increased limunance
-            newR = newR * 255
-            newG = newG * 255
-            newB = newB * 255
-            
-            # set the new color of the window
-            adjustedColor = Color(math.trunc(newR), math.trunc(newG), math.trunc(newB))
-            myTARDIS.backWindow.setColor(adjustedColor)
-            time.sleep(delay)
-
+signal.signal(signal.SIGINT, signal_handler)
 
 myTARDIS = TARDIS("Mike Nibeck", BLUE)
-myTARDIS.backWindow.setColor(BLUE)
-# myTARDIS.turnOn
-print(myTARDIS.backWindow.color.r, myTARDIS.backWindow.color.g, myTARDIS.backWindow.color.b)
-print("Backwindow brightness: ", myTARDIS.backWindow.brightness)
-# myTARDIS.backWindow.setColor(RED)
-time.sleep(1)
-# myTARDIS.backWindow.setBrightness(50)
-time.sleep(1)
-
-# myTARDIS.backWindow.flash(5, .1)
-# myTARDIS.backWindow.pulseWindow(.005, 2)
-# myTARDIS.frontWindow.setColor(blue, 100)
-# myTARDIS.frontWindow.pulseWindow(.005, 2)
+print("After TARDIS create")
+myTARDIS.backWindow.color = BLUE
+myTARDIS.backWindow.turnOn()
+myTARDIS.backWindow.pulseWindow(2, .005)
+myTARDIS.backWindow.color = RED
+myTARDIS.backWindow.pulseWindow(2, .005)
+myTARDIS.backWindow.color = PURPLE
+myTARDIS.backWindow.pulseWindow(2, .005)
+myTARDIS.backWindow.color = YELLOW
+myTARDIS.backWindow.pulseWindow(2, .005)
+myTARDIS.backWindow.flash(5, .1)
 
 time.sleep(1)
 myTARDIS.turnOff()
